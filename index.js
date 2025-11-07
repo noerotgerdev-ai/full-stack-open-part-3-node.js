214b60e59e6c8492aac0ee22a737e71bab6b7637
Noelia Rotger <noerotgerdev@gmail.com>
2025-11-07 20:17:00 +0100

    part3 phonebook 3.3

--- index.js
+++ index.js
@@ -1,50 +1,58 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 const list = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/', (req, res) => {
-    res.send('<h1>Node.js APP</h1>')
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Node.js APP</h1>");
+});
 
-app.get('/api/persons', (req, res) => {
-    res.json(list)
-})
+app.get("/api/persons", (req, res) => {
+  res.json(list);
+});
 
-app.get('/info', (req, res) => {
-    const contacts = list.length
-    const date = new Date()
-
-    const message = `
+app.get("/info", (req, res) => {
+  const contacts = list.length;
+  const date = new Date();
+  const message = `
     <p>Phonebook has info for ${contacts} people</p>
     <p>${date}</p>
-    `
+    `;
 
-    res.send(message)
+  res.send(message);
+});
+
+app.get('/api/persons/:id', (req, res) =>{
+    const id = Number(req.params.id)
+
+    const contact = list.find(item => item.id === id)
+
+    if(!contact) return res.status(404).send(`The person with id ${id} doesn't exists`)
+    res.send(contact)
 })
 
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`App running in http://localhost:${PORT}`)
-})
+  console.log(`App running in http://localhost:${PORT}`);
+});
