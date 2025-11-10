b193e4a434681e7a0cb0517b5d054a2cee4cd5e8
Noelia Rotger <noerotgerdev@gmail.com>
2025-11-11 00:19:47 +0100

    part3 phonebook 3.11(DEPLOY)

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token("msg", function (req) {
   const message = JSON.stringify(req.body);
